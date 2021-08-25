54d901f00307f8071f38620f6c4832926f0a9233
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2021-08-25 21:54:31 +0400

    add panorama image

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// dark color 
+const checkbox = document.getElementById('checkbox');
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+// end dark color
+
+
+
+
+
+
+// const panoImage = document.querySelector('.pano-image');
+// const miamiPano = "assets/images/road.jpg";
+// const panorama = new PANOLENS.imagePanorama(miamiPano);
+// const viewer = new PAROLENS.Viewer({
+//     container: panoImage,
+//     autoRotate: true,
+//     autoRotateSpeed: 0.2
+// });
+// viewer.add(panorama);
+
