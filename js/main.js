a60035e65050942e8eedd397ecadcb7bbb0b68d8
MariQerdika <40671485+MariQerdika@users.noreply.github.com>
2021-08-26 21:45:12 +0400

    ../

--- js/main.js
+++ js/main.js
@@ -20,3 +20,37 @@ checkbox.addEventListener('change', () => {
 // });
 // viewer.add(panorama);
 
+
+
+// const progText = document.querySelectorAll(".progText");
+// const progress = document.querySelectorAll(".progress");
+// const progContainer = document.querySelector(".container");
+// let bol = false;
+
+
+// window.addEventListener("scroll", function(){
+//     if (pageYOffset > progContainer.offsetTop - 600 && bol === false){
+//         for (let i = 0; i < progText.length; i++){
+//             progText[i].innerText = 0;
+//             count = 0;
+
+//             progress[i].style.bottom = "100%";
+
+//             progress[i].style.bottom = progText[i].dataset.count - 100 + "%";
+
+
+//             function updateCount(){
+//                 target = parseInt(progText[i].dataset.count);
+//                 if (count < target) {
+//                     count++;
+//                     progText[i].innerText = count;
+//                     setTimeout(updateCount, 50);
+//                 } else {
+//                     progText[i].innerText = target + "%";
+//                 }
+//             }
+//             updateCount();
+//             bol = true;
+//         }
+//     }
+// })
